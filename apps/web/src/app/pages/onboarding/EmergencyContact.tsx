2fcff4fbd92074877848f4dcbeff08466cf5f6b9
marqnetworks <meetezri@gmail.com>
2026-03-05 02:36:22 +0500

    fix

--- apps/web/src/app/pages/onboarding/EmergencyContact.tsx
+++ apps/web/src/app/pages/onboarding/EmergencyContact.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
 import { Card } from "../../components/ui/card";
 import { Textarea } from "../../components/ui/textarea";
+import { Label } from "../../components/ui/label";
 import {
   Dialog,
   DialogContent,
